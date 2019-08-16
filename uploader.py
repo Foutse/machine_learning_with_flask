93696ab5b9a2cf68d439fb1bb90ca72fe6f25f84
gbazack <morenzack@gmail.com>
2019-08-16 19:50:00 +0100

    Initial commit

--- uploader.py
+++ uploader.py
@@ -1,25 +1,44 @@
 import os
 from flask import Flask, render_template, request
+#from sklearn.externals import joblib
+import traceback
+import pandas as pd
+import numpy as np
 from werkzeug import secure_filename
+import	playsound as playsound
+from playsound import playsound
+
 app = Flask(__name__)
 
-UPLOAD_FOLDER='Path_to/uploader/'
+UPLOAD_FOLDER='uploader/'
 
 @app.route('/')
 def upload():
 	return render_template('upload.html')
 
-	
+#Upload file from the web interface	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def uploader():
 	if request.method == 'POST':
-		f = request.files['file']
-		absolute_file = os.path.abspath(UPLOAD_FOLDER + f.filename)		
-		f.save(absolute_file)
-		
+		audio = request.files['audio']
+		absolute_file = os.path.abspath(UPLOAD_FOLDER + audio.filename)		
+		audio.save(absolute_file)
+		playsound(absolute_file)
 	
 	return absolute_file
-	
+
+"""
+@app.route('/predict')
+def predict():
+	audio=uploader()
+	if audio:
+		try:
+			playsound(audio)
+		except SoundError:
+			return("Not an audio file! Please upload an audio file")
 		
+	return 0
+"""
+
 if __name__ == '__main__':
-	app.run(port=6003, debug = True)
+	app.run(port=10000, debug = True)
